5a06731d382438c88b9069b0c45f2fc452281142
Arby The Coder <houseofarby@gmail.com>
2025-11-23 16:55:08 +0100

    feat: Add Shopping Cart with quantity controls and order summary

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import ContactUs from './pages/ContactUs'
 import ClientVendorStorefront from './pages/customer/ClientVendorStorefront' // Client-exact vendor storefront
 import CustomerDashboard from './pages/customer/CustomerDashboard'
 import ProductBrowsing from './pages/customer/ProductBrowsing'
+import ShoppingCart from './pages/customer/ShoppingCart'
 import Checkout from './pages/customer/Checkout'
 import OrderConfirmation from './pages/customer/OrderConfirmation'
 import OrderTracking from './pages/customer/OrderTracking'
@@ -76,6 +77,7 @@ function AppContent() {
       <Route path="/delivery" element={<Delivery />} />
       <Route path="/contact" element={<ContactUs />} />
       <Route path="/products" element={<ProductBrowsing />} />
+      <Route path="/cart" element={<ShoppingCart />} />
 
       {/* Customer Dashboard */}
       <Route
